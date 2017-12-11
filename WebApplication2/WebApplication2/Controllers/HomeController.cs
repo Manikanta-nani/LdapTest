b50948776d6582a53169d0480fcba556ae86e818
DESKTOP-GREJEKS\Nani <manikanta.mvc.asp@gmail.com>
2017-12-11 08:53:28 +0530

    test code

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+
+            string ldapurl = "";
+            IsAuthenticated(ldapurl, model.UserName, model.Password);
+
+            return View();
+        }
+
+
+        public static bool IsAuthenticated(string ldap, string usr, string pwd)
+        {
+            bool authenticated = false;
+
+            try
+            {
+                DirectoryEntry entry = new DirectoryEntry(ldap, usr, pwd);
+                object nativeObject = entry.NativeObject;
+                authenticated = true;
+            }
+            catch (DirectoryServicesCOMException cex)
+            {
+                Console.WriteLine(cex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return authenticated;
+        }
+
+    }
+}
